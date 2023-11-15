f708eccd24d6d17ac88da8679562b00b41a6756c
MarceloRodrigues1853 <marcelo180886@gmail.com>
2023-11-15 10:48:56 -0300

    adição de comenta´rios

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -20,6 +20,7 @@ let abrirCards = [];
 
 let embaralharEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
+// Criação dinâmica das cartas no jogo
 for (let i = 0; i < emojis.length; i++) {
   let box = document.createElement("div");
   box.className = "item";
@@ -28,6 +29,7 @@ for (let i = 0; i < emojis.length; i++) {
   document.querySelector(".game").appendChild(box);
 }
 
+// Função chamada quando uma carta é clicada
 function handleClick() {
   if (abrirCards.length < 2) {
     this.classList.add("boxOpen");
@@ -41,6 +43,7 @@ function handleClick() {
   console.log(abrirCards);
 }
 
+// Função para verificar se as duas cartas abertas correspondem
 function checkMatch() {
   if (abrirCards[0].innerHTML === abrirCards[1].innerHTML) {
     abrirCards[0].classList.add("boxMatch");
@@ -52,6 +55,7 @@ function checkMatch() {
 
   abrirCards = [];
 
+  // Verifica se todas as cartas têm correspondência (jogo concluído)
   if (document.querySelectorAll(".boxMatch").length === emojis.length) {
     alert("Você venceu !");
   }
